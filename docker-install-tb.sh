1f5832da9a9dbb31d874d64040456a91b3b71b23
Sergey Matvienko <smatvienko@thingsboard.io>
2022-06-23 21:47:32 +0300

    docker-compose multiline refactor for better merge

--- docker-install-tb.sh
+++ docker-install-tb.sh
@@ -73,7 +73,8 @@ cd $DEPLOYMENT_FOLDER
 if [ ! -z "${ADDITIONAL_STARTUP_SERVICES// }" ]; then
     docker-compose \
       --env-file ../.env \
-      -f docker-compose.yml $ADDITIONAL_CACHE_ARGS $ADDITIONAL_COMPOSE_ARGS $ADDITIONAL_COMPOSE_QUEUE_ARGS up -d $ADDITIONAL_STARTUP_SERVICES
+      -f docker-compose.yml $ADDITIONAL_CACHE_ARGS $ADDITIONAL_COMPOSE_ARGS $ADDITIONAL_COMPOSE_QUEUE_ARGS \
+      up -d $ADDITIONAL_STARTUP_SERVICES
 fi
 
 docker-compose \
